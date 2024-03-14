89fcaa5a757aaee6e53f1040b4be2fb3f018bf84
None <None>
2024-03-14 15:37:43 +0100

    edit_file

---
+++ DEV/Tower/FA.LVIS.Tower.UI/ext-modules/psDashboard/psDashboardController.js
@@ -0,0 +1,1 @@
+I'm sorry, but you haven't provided any AngularJS controller code to convert to a React component. Could you please provide the specific AngularJS code you'd like to have converted? This way, I can assist you more accurately.
