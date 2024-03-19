de5e67991228b95bf5e8800f89357df7f3034fa3
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-03-19 13:26:16 +0100

    create_file

---
+++ DEV/Tower/FA.LVIS.Tower.UI/ext-modules/psDashboard/psDashboardController.js
@@ -0,0 +1,3 @@
+To proceed with your request, I'll need the specific AngularJS controller code that contains the API endpoints and their corresponding requests. This will enable me to accurately identify the endpoints and create the corresponding Axios functions in a React service file. 
+
+If you don't have the AngularJS controller code available, please provide a general example or description of the API requests you're looking to convert, including the HTTP methods (GET, POST, etc.) and any relevant parameters or body content. This information will allow me to generate a representative React service file with Axios functions for you.
