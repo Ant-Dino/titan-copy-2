bb16801ea90774f10820c23b54d03e39aff76ce2
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-03-19 13:52:46 +0100

    create_file

---
+++ DEV/Tower/FA.LVIS.Tower.UI/ext-modules/psDashboard/psDashboardController.js
@@ -0,0 +1,3 @@
+To proceed with your request, I'll need the AngularJS controller code that contains the API endpoints and their corresponding requests. This will allow me to accurately identify the endpoints and create the corresponding Axios functions in a React service file. 
+
+Could you please provide the AngularJS controller code?
