5ce997f3b8d816e00292871f3de0577eb7a77120
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-03-20 15:20:42 +0100

    create_file_with_context

---
+++ DEV/Tower/FA.LVIS.Tower.UI/src/services/psDashboard.service.ts
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+const dashboardService = {
+  loadBEQExceptions() {
+    return axios.get('Dashboard/BEQException/')
+      .then(response => response.data)
+      .catch(error => console.error("There was an error loading the BEQ exceptions:", error));
+  },
+
+  loadTEQExceptions() {
+    return axios.get('Dashboard/TEQException/')
+      .then(response => response.data)
+      .catch(error => console.error("There was an error loading the TEQ exceptions:", error));
+  },
+
+  loadGraphicalTEQException() {
+    return axios.get('Dashboard/GraphicalTEQException/')
+      .then(response => response.data)
+      .catch(error => console.error("There was an error loading the graphical TEQ exceptions:", error));
+  },
+
+  loadGraphicalBEQException() {
+    return axios.get('Dashboard/GraphicalBEQException/')
+      .then(response => response.data)
+      .catch(error => console.error("There was an error loading the graphical BEQ exceptions:", error));
+  }
+};
+
+export default dashboardService;
