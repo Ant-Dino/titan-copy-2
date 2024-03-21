50dd9421ee06879a4f228c3561274d4b9c24c664
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-03-21 12:00:17 +0100

    rename_file

--- DEV/Tower/FA.LVIS.Tower.UI/src/App.tsx
+++ DEV/Tower/FA.LVIS.Tower.UI/src/App.tsx
@@ -1,26 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, {useState, useEffect} from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from "react-router-dom";
+import Dashboard from './components/Dashboard';
+import Reporting from './components/Reporting';
+import Auditing from './components/Auditing';
+import Security from './components/Security';
+import ManageExternalRefNumber from './components/ManageExternalRefNumber';
+import ManageServiceRequest from './components/ManageServiceRequest';
+import EndPointAccess from './components/EndPointAccess';
+import ConfirmServiceRequest from './components/ConfirmServiceRequest';
+import ProviderMappings from './components/ProviderMappings';
+import ProductProviderMappings from './components/ProductProviderMappings';
+import FastOfficeMappings from './components/FastOfficeMappings';
+import FastFilePreferenceMappings from './components/FastFilePreferenceMappings';
+import CustomerMappings from './components/CustomerMappings';
+import OutDocumentMappings from './components/OutDocumentMappings';
+import InDocumentMappings from './components/InDocumentMappings';
+import LocationMappings from './components/LocationMappings';
+import WebhooksMappings from './components/WebhooksMappings';
+import CategoryMappings from './components/CategoryMappings';
+import FastGabMappings from './components/FastGabMappings';
+import OutEventMappings from './components/OutEventMappings';
+import Exceptions from './components/Exceptions';
+import BusinessException from './components/BusinessException';
+import FastWebOrders from './components/FastWebOrders';
+import TerminalLogInformation from './components/TerminalLogInformation';
+import FastWorkFlowMappings from './components/FastWorkFlowMappings';
+import FastTaskMappings from './components/FastTaskMappings';
+import FastToLvisDocMappings from './components/FastToLvisDocMappings';
+import LvisToFastDocMappings from './components/LvisToFastDocMappings';
+import Subscription from './components/Subscription';
+import ContactMappings from './components/ContactMappings';
+import ContactProviderMappings from './components/ContactProviderMappings';
+import AccessRequest from './components/AccessRequest';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/dashboard" component={Dashboard} />
+          <Route path="/reporting" component={Reporting} />
+          <Route path="/auditing" component={Auditing} />
+          <Route path="/security" component={Security} />
+          <Route path="/manageexternalrefnumber" component={ManageExternalRefNumber} />
+          <Route path="/manageservicerequest" component={ManageServiceRequest} />
+          <Route path="/endpointaccess" component={EndPointAccess} />
+          <Route path="/confirmservicerequest" component={ConfirmServiceRequest} />
+          <Route path="/providermappings" component={ProviderMappings} />
+          <Route path="/providermappings/:ExternalID" component={ProviderMappings} />
+          <Route path="/productprovidermappings/:ProviderID" component={ProductProviderMappings} />
+          <Route path="/fastofficemappings/:ExternalID" component={FastOfficeMappings} />
+          <Route path="/fastfilepreferencemappings" component={FastFilePreferenceMappings} />
+          <Route path="/Customermappings" component={CustomerMappings} />
+          <Route path="/Customermappings/:CustomerName" component={CustomerMappings} />
+          <Route path="/outdocmappings/:LenderName/:isGroups" component={OutDocumentMappings} />
+          <Route path="/indocmappings" component={InDocumentMappings} />
+          <Route path="/Locationmappings/:CustomerName" component={LocationMappings} />
+          <Route path="/webhooksMappings/:CustomerName" component={WebhooksMappings} />
+          <Route path="/Categorymappings" component={CategoryMappings} />
+          <Route path="/fastgabmappings/:LocationName" component={FastGabMappings} />
+          <Route path="/outeventmappings/:LenderName" component={OutEventMappings} />
+          <Route path="/Exceptions/:ExceptionType?" component={Exceptions} />
+          <Route path="/businessexception" component={BusinessException} />
+          <Route path="/fastweborders" component={FastWebOrders} />
+          <Route path="/terminalloginformation" component={TerminalLogInformation} />
+          <Route path="/FastWorkFlowMappings" component={FastWorkFlowMappings} />
+          <Route path="/FastTaskMappings" component={FastTaskMappings} />
+          <Route path="/FastToLVISDocMappings" component={FastToLvisDocMappings} />
+          <Route path="/LVISToFastDocMappings" component={LvisToFastDocMappings} />
+          <Route path="/subscription/:CustomerName/:isCategory" component={Subscription} />
+          <Route path="/Contactmappings/:LocationName" component={ContactMappings} />
+          <Route path="/ContactProvidermappings/:CustomerId" component={ContactProviderMappings} />
+          <Route path="/AccessRequest" component={AccessRequest} />
+          <Route path="/AccessRequest/:emailid" component={AccessRequest} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
-
 export default App;
