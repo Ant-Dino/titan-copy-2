42a8780cf00799321d8fd912d8ed76aa41b1d022
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-03-28 10:29:43 +0100

    create_file_with_import_context: psAuditingController.js Input File: DEV/Tower/FA.LVIS.Tower.UI/ext-modules/psAuditing/psAuditingController.js Max Tries: 1 Arguments Provided: create_file_prompt, file_path_info_prompt, edit_prompt, path_to_file Tool Execution Time: 63.1792s
    Git Stats:
    Files Changed: 1
    Line Insertions: 8
    Line Deletions: 21

--- DEV/Tower/FA.LVIS.Tower.UI/ext-modules/psAuditing/psAuditingController.js
+++ DEV/Tower/FA.LVIS.Tower.UI/ext-modules/psAuditing/psAuditingController.js
@@ -1,12 +1,9 @@
- 
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import { Modal, Button } from 'react-bootstrap';
 import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
 import { growl } from '@craco/craco'; // Assuming growl functionality is handled similar
-import { GetAuditDetails, GetAuditDetailsFilter } from './services/psAuditingApiUri';
-
+import { getUserInfo, getAuditDetails, getAuditDetailsFilter } from 'DEV/Tower/FA.LVIS.Tower.UI/src/services/psAuditing.service';
 const PsAuditingComponent = () => {
   const [accessRights, setAccessRights] = useState(); 
   const [canManageTEQ, setCanManageTEQ] = useState(false);
@@ -17,25 +14,21 @@ const PsAuditingComponent = () => {
   const [disableDate, setDisableDate] = useState(true);
   const [validateError, setValidateError] = useState(false);
   const [auditData, setAuditData] = useState([]);
-
   useEffect(() => {
-    // Simulate UserInfo.getUser() still using promises
-    axios.get('/api/userinfo').then((response) => {
-      const { ActivityRight, CanManageTEQ, CanManageBEQ } = response.data;
+    getUserInfo().then((response) => {
+      const { ActivityRight, CanManageTEQ, CanManageBEQ } = response;
       setAccessRights(ActivityRight);
       setCanManageTEQ(CanManageTEQ);
       setCanManageBEQ(CanManageBEQ);
       if (ActivityRight !== 'Admin' && ActivityRight !== 'SuperAdmin') {
-        // Here you would redirect or show modal similar to the $uibModal.open logic
         alert('You are not authorized to view this page.');
       }
     });
   }, []);
-
   const search = () => {
     if (dateFilterSelection !== '1') {
-      GetAuditDetailsFilter(dateFilterSelection).then((response) => {
-        setAuditData(response.data);
+      getAuditDetailsFilter({filterOption: dateFilterSelection}).then((response) => {
+        setAuditData(response);
       }).catch((error) => {
         growl.error(error.data);
       });
@@ -44,14 +37,13 @@ const PsAuditingComponent = () => {
         growl.error("Please correct the dates");
         return;
       }
-      GetAuditDetails({ fromDate, throughDate }).then((response) => {
-        setAuditData(response.data);
+      getAuditDetails({ fromDate, throughDate }).then((response) => {
+        setAuditData(response);
       }).catch((error) => {
         growl.error(error.data);
       });
     }
   };
-
   const validateDates = () => {
     if (throughDate < fromDate) {
       setValidateError(true);
@@ -59,21 +51,17 @@ const PsAuditingComponent = () => {
     }
     return true;
   };
-
   const handleFromDateChange = (date) => {
     setFromDate(date);
     validateDates();
   };
-
   const handleThroughDateChange = (date) => {
     setThroughDate(date);
     validateDates();
   };
-
   const changeSelect = (item) => {
     setDisableDate(item !== '1');
   };
-
   return (
     <div>
       <h2>Audit Management</h2>
@@ -86,5 +74,4 @@ const PsAuditingComponent = () => {
     </div>
   );
 };
-
 export default PsAuditingComponent;
